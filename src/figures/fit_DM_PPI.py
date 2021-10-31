ee421dbf1871f2d47b7a9523190180a2697fa3c7
math <mathren90@gmail.com>
2021-10-31 13:23:30 -0400

    emacs compiles pdf

--- src/figures/fit_DM_PPI.py
+++ src/figures/fit_DM_PPI.py
@@ -1,10 +1,15 @@
 # Masses of compact remnant  from CO core masses
 __author__ = "M. Renzo (mrenzo@flatironinstitute.org)"
 
+# for fit
 import numpy as np
-import sys
 import scipy
 from scipy.optimize import curve_fit
+# for plot
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+
 
 def linear(x, a, b):
     return a*x+b
@@ -40,7 +45,7 @@ if __name__ == "__main__":
 
     datafile = "../data/datafile1.txt"
     # src, col = get_src_col(datafile)
-    src, col = get_select_datafile_range(datafile)
+    # src, col = get_select_datafile_range(datafile)
 
     fig=plt.figure(figsize=(12,20))
     gs = gridspec.GridSpec(7, 1)
@@ -53,47 +58,51 @@ if __name__ == "__main__":
     ax6 = fig.add_subplot(gs[5])
     ax7 = fig.add_subplot(gs[6])
     axes = [ax1,ax2,ax3,ax4,ax5,ax6,ax7]
-
-    rainbow = plt.cm.rainbow(np.linspace(0,1,8))
-
-    # --------------------------------------------------------------------------------------
-    # fit happens here!
-    # load data
-    Mco = src[:, col.index("Mco")]
-    Z = src[:, col.index('Z')]
-    Mhe = src[:, col.index('Mhe')]
-    dMpulse = src[:, col.index('dMpulse')]
-    # fit only in the PPISN range -- neglect the Z dependence of this range itself
-    ind_for_fit = (Mco>=38) & (Mco<=60)
-    popt, pcov = curve_fit(fitting_func_Z, [Mco[ind_for_fit], Z[ind_for_fit]], dMpulse[ind_for_fit])
-    # print(popt)
-    fit = "$\Delta M_\mathrm{PPI} = ("+f"{popt[0]:.4f}"+r"\log_{10}(Z)+"+f"{popt[1]:.4f})"+r"\times (M_\mathrm{CO}+"+f"{popt[2]:.1f}"+")^3"+f"{popt[3]:.4f}"+r"\times (M_\mathrm{CO}+"+f"{popt[2]:.1f}"+")^2$"
-    ax1.set_title(fit, fontsize=20)
-    # --------------------------------------------------------------------------------------
-
-    for i, metallicity in enumerate(sorted(np.unique(Z))):
-        ax = axes[i]
-        ax.axhline(0, 0,1,lw='1', c='k', ls='--', zorder=0)
-        # first plot data
-        x = Mco[Z==metallicity]
-        y = dMpulse[Z==metallicity]
-        ax.scatter(x, y, color=rainbow[i], label=r"$Z="+f"{metallicity:.0e}"+"$")
-        # then plot fit
-        ind_for_fit = (x>=38) & (x<=60)
-        x = x[ind_for_fit]
-        ax.plot(x, fitting_func_Z([x,[metallicity]*len(x)],*popt), c=rainbow[i])
-        # larger range to show the fit
-        xx = np.linspace(30,60,1000)
-        yy = fitting_func_Z([xx,[metallicity]*len(xx)],*popt)
-        ax.plot(xx, yy, c=rainbow[i], ls="--", lw=8, alpha=0.5, zorder=0)
-        # ----------
-    ax.legend(fontsize=20, handletextpad=0.1, frameon=True)
-    ax.set_ylim(-5,42)
-    ax.set_xlim(30,75)
-    if ax != ax7:
-        ax.set_xticklabels([])
+    rainbow = plt.cm.rainbow(np.linspace(0,1,len(axes)))
+    for ax in axes:
+        ax.scatter(np.linspace(0,1,10), np.linspace(0,1,10), s=1e2*np.linspace(0,1,10), color=rainbow[axes.index(ax)])
 
 
-    ax4.set_ylabel(r"$\Delta M_\mathrm{PPI} \ [M_\odot]$")
-    ax7.set_xlabel(r"$M_\mathrm{CO} \ [M_\odot]$")
-    plt.savefig('DM_PPI.png')
+
+    # # --------------------------------------------------------------------------------------
+    # # fit happens here!
+    # # load data
+    # Mco = src[:, col.index("Mco")]
+    # Z = src[:, col.index('Z')]
+    # Mhe = src[:, col.index('Mhe')]
+    # dMpulse = src[:, col.index('dMpulse')]
+    # # fit only in the PPISN range -- neglect the Z dependence of this range itself
+    # ind_for_fit = (Mco>=38) & (Mco<=60)
+    # popt, pcov = curve_fit(fitting_func_Z, [Mco[ind_for_fit], Z[ind_for_fit]], dMpulse[ind_for_fit])
+    # # print(popt)
+    # fit = "$\Delta M_\mathrm{PPI} = ("+f"{popt[0]:.4f}"+r"\log_{10}(Z)+"+f"{popt[1]:.4f})"+r"\times (M_\mathrm{CO}+"+f"{popt[2]:.1f}"+")^3"+f"{popt[3]:.4f}"+r"\times (M_\mathrm{CO}+"+f"{popt[2]:.1f}"+")^2$"
+    # ax1.set_title(fit, fontsize=20)
+    # # --------------------------------------------------------------------------------------
+
+    # for i, metallicity in enumerate(sorted(np.unique(Z))):
+    #     ax = axes[i]
+    #     ax.axhline(0, 0,1,lw='1', c='k', ls='--', zorder=0)
+    #     # first plot data
+    #     x = Mco[Z==metallicity]
+    #     y = dMpulse[Z==metallicity]
+    #     ax.scatter(x, y, color=rainbow[i], label=r"$Z="+f"{metallicity:.0e}"+"$")
+    #     # then plot fit
+    #     ind_for_fit = (x>=38) & (x<=60)
+    #     x = x[ind_for_fit]
+    #     ax.plot(x, fitting_func_Z([x,[metallicity]*len(x)],*popt), c=rainbow[i])
+    #     # larger range to show the fit
+    #     xx = np.linspace(30,60,1000)
+    #     yy = fitting_func_Z([xx,[metallicity]*len(xx)],*popt)
+    #     ax.plot(xx, yy, c=rainbow[i], ls="--", lw=8, alpha=0.5, zorder=0)
+    #     # ----------
+    # ax.legend(fontsize=20, handletextpad=0.1, frameon=True)
+    # ax.set_ylim(-5,42)
+    # ax.set_xlim(30,75)
+    # if ax != ax7:
+    #     ax.set_xticklabels([])
+
+
+    # ax4.set_ylabel(r"$\Delta M_\mathrm{PPI} \ [M_\odot]$")
+    # ax7.set_xlabel(r"$M_\mathrm{CO} \ [M_\odot]$")
+
+    plt.savefig('fit_DM_PPI.pdf')
